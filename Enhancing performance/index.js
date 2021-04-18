3446beae76ef0365f3684d12c2e0e243e7e91c0b
Vinz <ballutvincent@gmail.com>
2021-04-18 17:44:51 +0200

    use webworker-thread

--- Enhancing performance/index.js
+++ Enhancing performance/index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const Worker = require("webworker-threads").Worker;
 
 const start = Date.now();
 
 app.get("/", (req, res) => {
-  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-    res.send(`Server listen the port :${port}`);
+  const worker = new Worker(function () {
+    this.onmessage = function () {
+      let counter = 0;
+      while (counter < 1e9) {
+        counter++;
+      }
+      postMessage(counter);
+    };
   });
+  worker.onmessage = function (message) {
+    console.log(message.data);
+    res.send("" + message.data);
+  };
+  worker.postMessage();
 });
 app.get("/fast", (req, res) => {
   res.send(`This is fast`);
