ab505a8c0adf129b9b96bdd7ea287a1d1b9142d9
Vinz <ballutvincent@gmail.com>
2021-04-18 09:22:46 +0200

    clustering in action

--- Enhancing performance/index.js
+++ Enhancing performance/index.js
@@ -3,6 +3,9 @@ const cluster = require("cluster");
 if (cluster.isMaster) {
   // cause index.js to be executed *again* but in child mode
   cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
 } else {
   //i'm a child, i'm going to act like a server and do nothing else
   const express = require("express");
@@ -17,6 +20,9 @@ if (cluster.isMaster) {
   app.get("/", (req, res) => {
     doWork(5000);
     res.send(`Server listen the port :${port}`);
+  });
+  app.get("/fast", (req, res) => {
+    res.send(`This is fast`);
   });
 
   app.listen(port);
