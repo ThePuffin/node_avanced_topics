b949040a4421f1f62af83855868d085c077c9f8c
Vinz <ballutvincent@gmail.com>
2021-04-18 09:47:15 +0200

    benchmark server performance

--- Enhancing performance/index.js
+++ Enhancing performance/index.js
@@ -1,8 +1,15 @@
+process.env.UV_THREADPOOL_SIZE = 1;
+
+//to test in terminal: ab -c 1 -n 1 localhost:3000/fast to make a request
 const cluster = require("cluster");
+const crypto = require("crypto");
+
 /*I the file beging executed in master mode ?*/
 if (cluster.isMaster) {
   // cause index.js to be executed *again* but in child mode
   cluster.fork();
+  cluster.fork();
+  // cluster.fork();
   // cluster.fork();
   // cluster.fork();
   // cluster.fork();
@@ -12,14 +19,13 @@ if (cluster.isMaster) {
   const app = express();
   const port = 3000;
 
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
+  const start = Date.now();
 
   app.get("/", (req, res) => {
-    doWork(5000);
-    res.send(`Server listen the port :${port}`);
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+      // console.log("1", Date.now() - start);
+      res.send(`Server listen the port :${port}`);
+    });
   });
   app.get("/fast", (req, res) => {
     res.send(`This is fast`);
